e013db44d83022b38c7d248128019708cd428552
Nick Fisher <nfisher110@gmail.com>
2017-12-09 12:36:31 -0500

    rename exceptions to errors

---
+++ src/errors.js
@@ -0,0 +1,66 @@
+class CustomError {
+  constructor(message) {
+    this.name = this.constructor.name;
+    this.message = message;
+    this.stack = (new Error(message)).stack;
+  }
+}
+CustomError.prototype = Object.create(Error.prototype);
+CustomError.prototype.constructor = CustomError;
+
+/**
+ * @class
+ * @name NotImplementedError
+ */
+class NotImplementedError extends CustomError {
+  constructor(message) {
+    super(message || 'Not Implemented');
+  }
+}
+
+/**
+ * @class
+ * @name PropertyValidationError
+ */
+class PropertyValidationError extends CustomError {
+  /**
+   * @constructor
+   * @param {String} - property name
+   * @param value
+   */
+  constructor(property, value, msg) {
+    const message = msg || `Value ${value} is not valid for property ${property}`;
+    super(message);
+    this.property = property;
+    this.value = value;
+  }
+}
+
+/**
+ * @class
+ * @name NoSerializerFoundError
+ */
+class NoSerializerFoundError extends CustomError {
+  constructor(contentType) {
+    super(`No serializer found for content type ${contentType}`);
+    this.contentType = contentType;
+  }
+}
+
+/**
+ * @class
+ * @name NoDeserializerFoundError
+ */
+class NoDeserializerFoundError extends CustomError {
+  constructor(contentType) {
+    super(`No deserializer found for content type ${contentType}`);
+    this.contentType = contentType;
+  }
+}
+
+export default {
+  NotImplementedError,
+  PropertyValidationError,
+  NoSerializerFoundError,
+  NoDeserializerFoundError
+};
